bee53c86366be555145e944ac0599301b8823d83
kekke <kekke.dev@gmail.com>
2021-04-16 15:22:02 +0900

    設定ファイル更新

--- djangoWebSite/settings.py
+++ djangoWebSite/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # この行を追加
 ]
 
 ROOT_URLCONF = 'djangoWebSite.urls'
